ed2487476ec65e71846e5d7565f989df6900a9bb
Andy <andy.kitt@simpleweb.co.uk>
2022-02-08 14:53:33 +0000

    add default header styles

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,10 +1,15 @@
-import { callContract } from "./contract";
-import { addNetwork, NETWORK_ID, BLOCK_EXPLORER_URL } from "./network";
+import { callContract, createContract } from "./contract";
+import {
+  addNetwork,
+  NETWORK_ID,
+  BLOCK_EXPLORER_URL,
+} from "./network";
 import { readyToTransact } from "./wallet";
 
 export {
   addNetwork,
   callContract,
+  createContract,
   NETWORK_ID,
   BLOCK_EXPLORER_URL,
   readyToTransact,
