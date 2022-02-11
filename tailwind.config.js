8b9c6eb6e663f8044c368de691dd853a135ec85a
Andy <andy.kitt@simpleweb.co.uk>
2022-02-11 15:29:19 +0000

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  mode: "jit",
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./forms/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
