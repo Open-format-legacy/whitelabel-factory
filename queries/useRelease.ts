105138fe94e82fc196b519bf1097be498ca09ab5
Andy <andy.kitt@simpleweb.co.uk>
2022-02-15 15:58:40 +0000

    pass contract address to useRelease query

--- queries/useRelease.ts
+++ queries/useRelease.ts
@@ -2,45 +2,44 @@ import { useQuery } from "react-query";
 
 import { request, gql } from "graphql-request";
 
-const endpoint =
-  "https://api.thegraph.com/subgraphs/name/tinypell3ts/music-factory";
+const endpoint = "https://api.thegraph.com/subgraphs/name/tinypell3ts/music-factory";
 
-export default function useRelease() {
-  return useQuery("release", async () => {
-    const { release } = await request(
-      endpoint,
-      gql`
-        query {
-          release(id: "0xb5c326773ee3d22051ea7a5be415c1c8f2276893") {
-            id
-            name
-            symbol
-            totalSold
-            maxSupply
-            totalEarnings
-            totalReleased
-            royaltiesPercentage
-            salePrice
-            image
-            audio
-            description
-            licence
-            stakeholders {
-              id
-              share
-            }
-            payouts {
-              id
-              amount
-              createdAtTimestamp
-            }
-            creator {
-              id
-            }
-          }
-        }
-      `
-    );
-    return release;
-  });
+export default function useRelease(address: string) {
+	return useQuery(["release", address], async () => {
+		const { release } = await request(
+			endpoint,
+			gql`
+				query {
+					release(id: "${address}") {
+						id
+						name
+						symbol
+						totalSold
+						maxSupply
+						totalEarnings
+						totalReleased
+						royaltiesPercentage
+						salePrice
+						image
+						audio
+						description
+						licence
+						stakeholders {
+							id
+							share
+						}
+						payouts {
+							id
+							amount
+							createdAtTimestamp
+						}
+						creator {
+							id
+						}
+					}
+				}
+			`
+		);
+		return release;
+	});
 }
