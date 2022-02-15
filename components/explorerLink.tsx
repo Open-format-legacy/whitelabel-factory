78b8a6bee7289c780a801890b86a936cc51b3751
Andy <andy.kitt@simpleweb.co.uk>
2022-02-15 15:36:20 +0000

    add truncate prop to explorer link component

--- components/explorerLink.tsx
+++ components/explorerLink.tsx
@@ -3,33 +3,29 @@ import { BLOCK_EXPLORER_URL } from "../helpers";
 import { Button } from "../components";
 
 interface ExplorerLinkProps {
-  address: string;
-  isTx?: boolean;
+	address: string;
+	isTx?: boolean;
+	truncate?: boolean;
 }
 
 export default function ExplorerLink({
-  address,
-  isTx = false,
+	address,
+	isTx = false,
+	truncate = true
 }: ExplorerLinkProps) {
-  return (
-    <a
-      target="_blank"
-      rel="noreferrer"
-      href={BLOCK_EXPLORER_URL(address, isTx)}
-    >
-      {isTx ? (
-        <Button>
-          <span>View Transaction</span>
-          <ExternalLinkIcon className="h-6 w-6" />
-        </Button>
-      ) : (
-        <div className="flex mx-2 hover:text-indigo-800">
-          <span>
-            {address.slice(0, 4)}...{address.slice(-4)}
-          </span>
-          <ExternalLinkIcon className="h-6 w-6" />
-        </div>
-      )}
-    </a>
-  );
+	return (
+		<a target="_blank" rel="noreferrer" href={BLOCK_EXPLORER_URL(address, isTx)}>
+			{isTx ? (
+				<Button>
+					<span>View Transaction</span>
+					<ExternalLinkIcon className="h-6 w-6" />
+				</Button>
+			) : (
+				<div className="mx-2 flex hover:text-indigo-800">
+					<span>{truncate ? `${address.slice(0, 4)}...${address.slice(-4)}` : address}</span>
+					<ExternalLinkIcon className="h-6 w-6" />
+				</div>
+			)}
+		</a>
+	);
 }
